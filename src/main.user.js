abd94e25d4d4e4a1c6380624081934794d528b83
Fabian Bucher <fabian.bucher@digitaspixelpark.com>
2021-07-01 13:48:33 +0200

    increase version number

--- src/main.user.js
+++ src/main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         roar Overtime Tracker
 // @namespace    https://pixelpark.com/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://timesheet.roar.pub/*
